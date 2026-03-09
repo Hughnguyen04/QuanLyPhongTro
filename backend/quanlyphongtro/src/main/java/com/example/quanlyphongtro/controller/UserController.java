4f749eaa59fa91a1a17bff10fd8a15887f2118ab
trickez <dacvu44@gmail.com>
2026-03-09 14:20:20 +0700

    API Quan ly tai khoan

--- backend/quanlyphongtro/src/main/java/com/example/quanlyphongtro/controller/UserController.java
+++ backend/quanlyphongtro/src/main/java/com/example/quanlyphongtro/controller/UserController.java
@@ -1,4 +1,42 @@
 package com.example.quanlyphongtro.controller;
 
+import com.example.quanlyphongtro.dto.UserDTO;
+import com.example.quanlyphongtro.model.User;
+import com.example.quanlyphongtro.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<UserDTO> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getUserById(@PathVariable Integer id){
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public UserDTO createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public UserDTO updateUser(@PathVariable Integer id, @RequestBody User userUpdate){
+        return userService.updateUser(id, userUpdate);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Integer id){
+        userService.deleteUser(id);
+        return "User deleted successfully!";
+    }
 }
--- backend/quanlyphongtro/src/main/java/com/example/quanlyphongtro/dto/UserDTO.java
+++ backend/quanlyphongtro/src/main/java/com/example/quanlyphongtro/dto/UserDTO.java
@@ -1,4 +1,49 @@
 package com.example.quanlyphongtro.dto;
 
+import com.example.quanlyphongtro.model.User;
+
 public class UserDTO {
+    private Integer userId;
+    private String username;
+    private User.Role role;
+    private Boolean isActive;
+
+    public UserDTO(Integer userId, String username, User.Role role, Boolean isActive){
+        this.userId = userId;
+        this.username = username;
+        this.role = role;
+        this.isActive = isActive;
+    }
+
+    //Getter & Setter
+    public Integer getUserId(){
+        return userId;
+    }
+    public void setUserId(Integer userId){
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public User.Role getRole() {
+        return role;
+    }
+
+    public void setRole(User.Role role) {
+        this.role = role;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
 }
